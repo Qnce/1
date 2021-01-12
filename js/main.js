dc9c46262bddd8869c11a25859de53fce1479b87
Qnce <77355768+Qnce@users.noreply.github.com>
2021-01-12 23:47:42 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.slider__inner').slick({
+
+        nextArrow: '<button type="button" class = "slick-button slick-next" ></button>',
+
+        prevArrow: '<button type="button"class= "slick-button slick-prev" ></button >',
+
+        infinite: false,
+
+    });
+
+});
